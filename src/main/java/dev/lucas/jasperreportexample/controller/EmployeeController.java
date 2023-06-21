9b31866dda82600e0a5fb24997535e9f0b78b67f
lucasruviaro <lucasruviaro@hotmail.com>
2023-06-21 14:13:26 -0300

    final commit

--- src/main/java/dev/lucas/jasperreportexample/controller/EmployeeController.java
+++ src/main/java/dev/lucas/jasperreportexample/controller/EmployeeController.java
@@ -3,11 +3,14 @@ package dev.lucas.jasperreportexample.controller;
 import dev.lucas.jasperreportexample.model.Employee;
 import dev.lucas.jasperreportexample.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
+import net.sf.jasperreports.engine.JRException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.FileNotFoundException;
 import java.util.List;
 
 @RestController
@@ -20,5 +23,11 @@ public class EmployeeController {
     public ResponseEntity<List<Employee>> findAll(){
         List<Employee> employees = employeeService.findAll();
         return new ResponseEntity<>(employees, HttpStatus.OK);
+    }
+
+    @GetMapping("/report/{format}")
+    public ResponseEntity<String> generateReport(@PathVariable String format) throws JRException, FileNotFoundException {
+       String report = employeeService.exportReport(format);
+       return new ResponseEntity<>(report, HttpStatus.OK);
     }
 }
--- src/main/java/dev/lucas/jasperreportexample/service/EmployeeService.java
+++ src/main/java/dev/lucas/jasperreportexample/service/EmployeeService.java
@@ -3,9 +3,16 @@ package dev.lucas.jasperreportexample.service;
 import dev.lucas.jasperreportexample.model.Employee;
 import dev.lucas.jasperreportexample.repository.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -15,5 +22,26 @@ public class EmployeeService {
 
     public List<Employee> findAll(){
        return employeeRepository.findAll();
+    }
+
+
+    public String exportReport(String reportFormat) throws FileNotFoundException, JRException {
+        String path = "C:\\Users\\Lucas Ruviaro\\Desktop\\jasper-report-example";
+        List<Employee> employees = employeeRepository.findAll();
+
+        File file = ResourceUtils.getFile("classpath:employees.jrxml");
+        JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
+        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(employees);
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("createdBy", "Lucas Ruviaro");
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
+        if (reportFormat.equalsIgnoreCase("html")) {
+            JasperExportManager.exportReportToHtmlFile(jasperPrint, path + "\\employees.html");
+        }
+        if (reportFormat.equalsIgnoreCase("pdf")) {
+            JasperExportManager.exportReportToPdfFile(jasperPrint, path + "\\employees.pdf");
+        }
+
+        return "report generated in path : " + path;
     }
 }
